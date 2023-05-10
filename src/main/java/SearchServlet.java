c5452809b4ff0cb6f7b5fd4650cffb57c25f87be
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-05-09 23:42:21 -0500

    servlets-exercise

--- src/main/java/SearchServlet.java
+++ src/main/java/SearchServlet.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
-    @Override
-    protected void doGet(httpServletRequest
-    req, httpServletResponse resp) throws
-            ServletException, IOException {
-    String searchTerm = req.getParameter
-    ("search");
-    resp.setContentType("text/html");
-    resp.getWriter().println("<h2>Search results for: " + searchTerm + "</h2>");
-    }
+//    @Override
+//    protected void doGet(httpServletRequest
+//    req, httpServletResponse resp) throws
+//            ServletException, IOException {
+//    String searchTerm = req.getParameter
+//    ("search");
+//    resp.setContentType("text/html");
+//    resp.getWriter().println("<h2>Search results for: " + searchTerm + "</h2>");
+//    }
 }
